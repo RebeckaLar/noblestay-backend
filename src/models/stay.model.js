f9607da666d19e0a31b35293516fd7088da3a97b
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-10-14 16:51:08 +0200

    Simple castle stay schema created

---
+++ src/models/stay.model.js
@@ -0,0 +1,21 @@
+//Skapa schema för boenden
+
+import mongoose from "mongoose";
+
+//Skapa ny instans av Schema, öppna upp den som objekt definiera den
+const staySchema = new mongoose.Schema({
+    //id automatiskt, mongodb ger alla id:n automatiskt ett understreck _id
+    title: String,
+    description: String,
+    price: Number
+})
+//^nu har jag skapat schema men inte modell ännu, har bara sagt hur datan ska se ut.
+//nu behöver kommunicera med db på ngt sätt
+
+//Skapa modell/konstruktor/klass som används för att kommunicera med db
+//och se till koppla ihop modellen med rätt collection
+const Stay = mongoose.model('Stay', staySchema)
+
+//HUR MODELLEN SKA ANVÄNDAS(ANVÄND INTE HÄR DOCK, FLYTTA TILL CONTROLLERS)
+// const newStay = Stay.create()
+export default Stay;
