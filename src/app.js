54b5f59c2ebed892e7fefdfe66aafedd83cede30
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-10-22 11:41:54 +0200

    Added booking model, able to CREATE and READ bookings

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 //Ska ta in alla routes
 import express from 'express'
 import stayRoutes from './routes/stays.route.js'
+import bookingRouter from './routes/bookings.route.js'
 import { errorHandler, notFound } from './middleware/error.middleware.js'
  //skapar en Express-app
 const app = express() //sparar den i variabeln app. variabeln behövs också för att starta servern
@@ -24,6 +25,7 @@ const app = express() //sparar den i variabeln app. variabeln behövs också fö
 app.use(express.json()) //middleware
 
 app.use('/api/stays', stayRoutes)
+app.use('/api/bookings', bookingRouter)
 
 app.use(notFound) // notFound
 app.use(errorHandler) // errorHandler
