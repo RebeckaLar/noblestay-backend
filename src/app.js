bdd6586b88935646233689b2fc803d7e70081861
Rebecka Larsson <rebecka.rl.larsson@gmail.com>
2025-11-01 13:55:20 +0100

    User model, router and controller added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import express from 'express'
 import stayRoutes from './routes/stays.route.js'
 // import bookingRouter from './routes/bookings.route.js'
 import { errorHandler, notFound } from './middleware/error.middleware.js'
+import userRoutes from './routes/user.route.js'
+
  //skapar en Express-app
 const app = express() //sparar den i variabeln app. variabeln behövs också för att starta servern
 
@@ -11,6 +13,7 @@ app.use(express.json()) //middleware
 
 app.use('/api/stays', stayRoutes)
 // app.use('/api/bookings', bookingRouter)
+app.use('/api/auth', userRoutes)
 
 app.use(notFound) // notFound
 app.use(errorHandler) // errorHandler
---
+++ src/constants/roles.js
@@ -0,0 +1,8 @@
+
+const ROLES = {
+    ADMIN: 'admin',
+    CASTLEOWNER: 'castleOwner',
+    RESIDENT: 'resident'
+}
+
+export default ROLES
